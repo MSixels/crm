d6bd338061865f02d1304f0783bd698f07f3d518
Matheus Sixel <msixel22@gmail.com>
2024-09-30 11:25:31 -0300

    att page recuperar senha

--- src/Pages/RecuperarSenha/RecuperarSenha.jsx
+++ src/Pages/RecuperarSenha/RecuperarSenha.jsx
@@ -1,40 +1,54 @@
 import { useState } from 'react';
 import { sendPasswordResetEmail } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom'; // Import para redirecionamento
 import { auth } from '../../services/firebaseConfig';
 import './RecuperarSenha.css';
-import LogoText from '../../imgs/logoText.svg'
+import LogoText from '../../imgs/logoText.svg';
 
 function RecuperarSenha() {
     const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isEmailSent, setIsEmailSent] = useState(false);
+    const navigate = useNavigate();
 
     const handlePasswordReset = async () => {
         try {
             await sendPasswordResetEmail(auth, email);
-            setMessage('Verifique seu e-mail para redefinir sua senha.');
+            setIsEmailSent(true);
+            setErrorMessage('');
         } catch (error) {
-            setMessage('Erro ao enviar o e-mail de redefinição. Verifique o endereço de e-mail.');
+            setErrorMessage('Erro ao enviar o e-mail de redefinição. Verifique o endereço de e-mail.');
         }
     };
 
     return (
-    <>
-        <img src={LogoText} alt="logo" />
         <div className="containerRecuperarSenha">
-            <h1>Recuperar Senha</h1>
-            <p>Informe seu e-mail cadastrado na plataforma. Caso ele exista, enviaremos um link para redefinição de sua senha.</p>
-            <input 
-                type="email" 
-                placeholder="Digite seu e-mail" 
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <a href="">Acessar minha conta</a>
-            <button onClick={handlePasswordReset}>Enviar link de recuperação</button>
-            {message && <p>{message}</p>}
-            <p>Precisa de ajuda? <a href="">Fale conosco</a></p>
+            <img src={LogoText} alt="logo" />
+            
+            {!isEmailSent ? (
+                <div className="divContentRecovery">
+                    <h1>Recuperar Senha</h1>
+                    <p>Informe seu e-mail cadastrado na plataforma. Caso ele exista, enviaremos um link para redefinição de sua senha. Cheque sua caixa de entrada e também a de SPAM.</p>
+                    <input 
+                        type="email" 
+                        placeholder="Digite seu e-mail" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                    <a className='linkRecovery' onClick={() => {navigate(`/`)}}>Acessar minha conta</a>
+                    <button className='btn1' onClick={handlePasswordReset}>Enviar link de recuperação</button>
+                    <p>Precisa de ajuda? <a className='linkRecovery' href="">Fale conosco</a></p>
+                </div>
+            ) : (
+                <div className="mailSending">
+                    <h2>Tudo Certo!</h2>
+                    <p>Nós te enviamos um e-mail com as instruções para recuperar sua conta.</p>
+                    <button className='btn1' onClick={() => {navigate(`/`)}}>Voltar para área de login</button>
+                    <button className='btn2' onClick={handlePasswordReset}>Reenviar link</button>
+                </div>
+            )}
         </div>
-        </>
     );
 }
 
