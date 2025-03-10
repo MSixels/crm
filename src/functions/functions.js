cf642b4f8c02ec2e09a9d6e8d23f9ab50ab0c4d1
Geovanne Callegaro <callegarog00@gmail.com>
2025-03-10 15:54:12 -0300

    criação de aluno pelo admin

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -676,3 +676,28 @@ export async function updateUserCertificado(userId, name, matricula, cpf) {
         console.error("Erro ao atualizar os dados do usuário:", error);
     }
 }
+
+export function isValidCPF(cpf) {
+    cpf = cpf.replace(/\D/g, '');
+  
+    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+      return false; 
+    }
+  
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+      sum += Number(cpf[i]) * (10 - i);
+    }
+    let firstDigit = (sum * 10) % 11;
+    if (firstDigit === 10) firstDigit = 0;
+    if (firstDigit !== Number(cpf[9])) return false;
+  
+    sum = 0;
+    for (let i = 0; i < 10; i++) {
+      sum += Number(cpf[i]) * (11 - i);
+    }
+    let secondDigit = (sum * 10) % 11;
+    if (secondDigit === 10) secondDigit = 0;
+  
+    return secondDigit === Number(cpf[10]);
+  }
